4f68119fb8a66fa4ed209ce6dd6235696b4155ee
kyradrake <kyra.drake2014@gmail.com>
2017-02-09 09:39:24 -0600

    added queue of messages to write to userdata.h

--- UserData.h
+++ UserData.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 #include <string>
 #include <sstream>
 #include <vector>
@@ -9,15 +10,18 @@ class UserData {
 public:
     string name;
     vector<string> usersConnectedTo;
+    queue<string> messagesToWrite;
     
     UserData(void){
         name = "";
         usersConnectedTo = vector<string>();
+        messagesToWrite = queue<string>();
     }
     
     UserData(string user){
         name = user;
         usersConnectedTo = vector<string>();
+        messagesToWrite = queue<string>();
     }
     
     void* addUser(string userToAdd) {
