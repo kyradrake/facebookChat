2123728f2b97436a7d110fb02b82c323e10c8c50
kyradrake <kyra.drake2014@gmail.com>
2017-02-08 23:39:08 -0600

    works better

--- fbc.cpp
+++ fbc.cpp
@@ -169,69 +169,28 @@ public:
         }
     }
     
-    void sendChat() {           //string message, string dateAndTime
-        //SendChatToServer(ChatMessage) returns (ChatReply) {}
-        //ClientContext context;
-        /*
-        // create chat message and reply objects
-        ChatMessage chatMessage;
-        ChatReply reply;
-        
-        // set message username, time and date, and message
-        chatMessage.set_username(username);
-        chatMessage.set_datetime(dateAndTime);
-        chatMessage.set_message(message);
-        
-        // send chat message to server
+    void sendChat() {         
+
         ClientContext context;
-        Status status = stub->SendChatToServer(&context, chatMessage, &reply);
-        
-        // check if request was successful
-        if (!status.ok()) {
-            cout << "Error Occured: Message Could Not Send to Server.\n";
-        }
-        
-        */
-        /*
-        shared_ptr<ClientReaderWriter<ChatMessage, ChatMessage>> chatStream(stub->ChatStream(&context));
         
         static string name = username;
-        
-        thread chatSendThread([chatStream]() {
-            ChatMessage chatMessage;
-            string msg;
-            
-            getline(cin, msg);
-            chatMessage.set_username(name);
-            chatMessage.set_message(msg);
-            
-            chatStream->Write(chatMessage);
-        });
-        */
-        
-        ClientContext context;
 
         shared_ptr<ClientReaderWriter<ChatMessage, ChatMessage> > stream(
             stub->ChatStream(&context));
 
         thread writer([stream]() {
-            /*vector<ChatMessage> notes{
-                MakeRouteNote("First message", 0, 0),
-                MakeRouteNote("Second message", 0, 1),
-                MakeRouteNote("Third message", 1, 0),
-                MakeRouteNote("Fourth message", 0, 0)};
-                */
-            /*for (const RouteNote& note : notes) {
-                std::cout << "Sending message " << note.message()
-                          << " at " << note.location().latitude() << ", "
-                          << note.location().longitude() << std::endl;
-                stream->Write(note);
-            }*/
-            ChatMessage msg;
-            msg.set_username("test");
-            msg.set_message("pls work");
-            msg.set_datetime("ft");
-            stream->Write(msg);
+            while (true) {
+                string msg;
+                getline(cin, msg);
+                
+                ChatMessage ChatMsg;
+                ChatMsg.set_username(name);
+                ChatMsg.set_message(msg);
+                ChatMsg.set_datetime("");
+                
+                stream->Write(ChatMsg);
+            }
+            
             
             //stream->WritesDone();
         });
@@ -248,41 +207,6 @@ public:
         if (!status.ok()) {
           std::cout << "RouteChat rpc failed." << std::endl;
         }
-        
-        
-/*            
-        std::shared_ptr<ClientReaderWriter<RouteNote, RouteNote> > stream(
-        stub_->RouteChat(&context));
-
-    std::thread writer([stream]() {
-      std::vector<RouteNote> notes{
-        MakeRouteNote("First message", 0, 0),
-        MakeRouteNote("Second message", 0, 1),
-        MakeRouteNote("Third message", 1, 0),
-        MakeRouteNote("Fourth message", 0, 0)};
-      for (const RouteNote& note : notes) {
-        std::cout << "Sending message " << note.message()
-                  << " at " << note.location().latitude() << ", "
-                  << note.location().longitude() << std::endl;
-        stream->Write(note);
-      }
-      stream->WritesDone();
-    });
-
-    RouteNote server_note;
-    while (stream->Read(&server_note)) {
-      std::cout << "Got message " << server_note.message()
-                << " at " << server_note.location().latitude() << ", "
-                << server_note.location().longitude() << std::endl;
-    }
-    writer.join();
-    Status status = stream->Finish();
-    if (!status.ok()) {
-      std::cout << "RouteChat rpc failed." << std::endl;
-    }
-        
-        */
-        
         
     }
 };
